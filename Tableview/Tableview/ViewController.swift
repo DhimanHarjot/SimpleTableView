7f52f63e0be036cad952fa3bc8a523942db6b62b
DhimanHarjot <harjotsingh1321998@gmail.com>
2023-03-27 13:50:33 -0400

    simple
    tableview

--- Tableview/Tableview/ViewController.swift
+++ Tableview/Tableview/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var myTable: UITableView!
+    var tableItems = ["Item 1", "Item 2", "Item 3", "Item 4"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -28,15 +29,15 @@ class ViewController: UIViewController {
 }
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-       return 5
+       return tableItems.count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = myTable.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = "Demo Cell\(indexPath.row)"
+        cell.textLabel?.text = tableItems[indexPath.row]
         return cell
     }
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print("Cell \(indexPath.row) select")
+        print("Selected \(tableItems[indexPath.row])")
     }
 }
 
